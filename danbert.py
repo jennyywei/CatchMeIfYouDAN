f467f621011773a889840475bdf1a21821115d9c
Jenny Wei <jenwwwww@gmail.com>
2024-06-01 16:09:07 -0700

    change padding from static to dynamic padding; add gpu support

--- danbert.py
+++ danbert.py
@@ -3,6 +3,7 @@ import torch
 from tqdm import tqdm
 from transformers import DistilBertForSequenceClassification, DistilBertTokenizer, pipeline
 from torch.utils.data import Dataset, DataLoader
+from torch.nn.utils.rnn import pad_sequence
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from itertools import combinations
 from pprint import pprint
@@ -20,11 +21,14 @@ results_dir = "results"
 MAX_LEN = 512
 TRAIN_BATCH_SIZE = 4
 VALID_BATCH_SIZE = 2
-EPOCHS = 150
+EPOCHS = 2
 LEARNING_RATE = 1e-05
 
+############################### DEVICE AND MODEL SET UP #######################################
+from torch import cuda
+device = 'cuda' if cuda.is_available() else 'cpu'
 tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-uncased")
-model = DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased")
+model = DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased").to(device)
 
 ############################## LOADING FILES AND TOOLS ##############################
 
@@ -62,7 +66,7 @@ class Deepset(Dataset):
             None,
             add_special_tokens=True,
             max_length=self.max_len,
-            padding='max_length',
+            padding=False,
             return_token_type_ids=True,
             truncation=True
         )
@@ -78,30 +82,22 @@ class Deepset(Dataset):
     def __len__(self):
         return self.len
 
-############################## RUN FINE-TUNING ##############################
+############################## TRAINING FUNCTION DEFINITIONS ##############################
 
-train = load_split("train")
-validation = load_split("validation")
+def collate_fn(batch):
+    ids = [item['ids'] for item in batch]
+    mask = [item['mask'] for item in batch]
+    targets = [item['targets'] for item in batch]
 
-training_set = Deepset(train, tokenizer, MAX_LEN)
-testing_set = Deepset(validation, tokenizer, MAX_LEN)
+    ids_padded = pad_sequence(ids, batch_first=True, padding_value=tokenizer.pad_token_id)
+    mask_padded = pad_sequence(mask, batch_first=True, padding_value=0)
+    targets = torch.stack(targets)
 
-train_params = {
-    'batch_size': TRAIN_BATCH_SIZE,
-    'shuffle': True,
-    'num_workers': 0, # TODO: change if running on GPU?
-}
-
-test_params = {
-    'batch_size': VALID_BATCH_SIZE,
-    'shuffle': True,
-    'num_workers': 0, # TODO: change if running on GPU?
-}
-
-training_loader = DataLoader(training_set, **train_params)
-testing_loader = DataLoader(testing_set, **test_params)
-loss_function = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(params =  model.parameters(), lr=LEARNING_RATE)
+    return {
+        'ids': ids_padded,
+        'mask': mask_padded,
+        'targets': targets
+    }
 
 def calculate_accuracy(big_idx, targets):
     n_correct = (big_idx==targets).sum().item()
@@ -114,9 +110,9 @@ def train(epoch):
     n_examples = 0
     model.train()
     for i, data in enumerate(tqdm(training_loader)):
-        ids = data['ids']
-        mask = data['mask']
-        targets = data['targets']
+        ids = data['ids'].to(device, dtype = torch.long)
+        mask = data['mask'].to(device, dtype = torch.long)
+        targets = data['targets'].to(device, dtype = torch.long)
 
         outputs = model(ids, mask).logits
         loss = loss_function(outputs, targets)
@@ -135,8 +131,7 @@ def train(epoch):
 
         optimizer.zero_grad()
         loss.backward()
-        # # When using GPU
-        # optimizer.step() 
+        # optimizer.step() # TODO: uncomment when running on GPU
 
     print(f'The Total Accuracy for Epoch {epoch}: {(n_correct*100) / n_examples}')
     epoch_loss = tr_loss / n_steps
@@ -145,8 +140,6 @@ def train(epoch):
     print(f"Training Accuracy Epoch: {epoch_accu}")
     return 
 
-for epoch in tqdm(range(EPOCHS)):
-    train(epoch)
 
 def valid(model, testing_loader):
     model.eval()
@@ -155,10 +148,10 @@ def valid(model, testing_loader):
     n_steps = 0
     n_examples = 0
     with torch.no_grad():
-        for _, data in enumerate(tqdm(testing_loader)):
-            ids = data['ids']
-            mask = data['mask']
-            targets = data['targets']
+        for i, data in enumerate(tqdm(testing_loader)):
+            ids = data['ids'].to(device, dtype = torch.long)
+            mask = data['mask'].to(device, dtype = torch.long)
+            targets = data['targets'].to(device, dtype = torch.long)
             outputs = model(ids, mask).logits
             loss = loss_function(outputs, targets)
             val_loss += loss.item()
@@ -168,7 +161,7 @@ def valid(model, testing_loader):
             n_steps += 1
             n_examples += targets.size(0)
             
-            if _%5000==0:
+            if i % 5000==0:
                 loss_step = val_loss / n_steps
                 accu_step = (n_correct*100) / n_examples
                 print(f"Validation Loss per 100 steps: {loss_step}")
@@ -179,6 +172,35 @@ def valid(model, testing_loader):
     print(f"Validation Accuracy Epoch: {epoch_accu}")
     
     return epoch_accu
+
+############################## RUN FINE-TUNING ##############################
+train_df = load_split("train")
+validation_df = load_split("validation")
+
+training_set = Deepset(train_df, tokenizer, MAX_LEN)
+testing_set = Deepset(validation_df, tokenizer, MAX_LEN)
+
+train_params = {
+    'batch_size': TRAIN_BATCH_SIZE,
+    'shuffle': True,
+    'num_workers': 0, # TODO: change if running on GPU?
+    'collate_fn': collate_fn
+}
+
+test_params = {
+    'batch_size': VALID_BATCH_SIZE,
+    'shuffle': True,
+    'num_workers': 0, # TODO: change if running on GPU?
+    'collate_fn': collate_fn
+}
+
+training_loader = DataLoader(training_set, **train_params)
+testing_loader = DataLoader(testing_set, **test_params)
+loss_function = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(params =  model.parameters(), lr=LEARNING_RATE)
+
+for epoch in tqdm(range(EPOCHS)):
+    train(epoch)
 
 accuracy = valid(model, testing_loader)
 print("Accuracy on test data = %0.2f%%" % accuracy)
